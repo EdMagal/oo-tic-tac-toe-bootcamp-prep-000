d949ee6d1652e3494295b71518dac897a29a27f1
Eduardo Magalhaes <ed83isme@gmail.com>
2018-02-23 18:04:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,9 @@
 class TicTacToe
+  
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+  
   WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,6 +14,8 @@ class TicTacToe
   [0, 4, 8],
   [2, 4, 6]
   ]
+  
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -19,15 +23,23 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  
   def input_to_index(user_input)
     @int_index = user_input.to_i - 1 
   end
+  
+  
   def move(int_index, token = "X")
     @board[int_index] = token
   end
+  
+  
   def position_taken?(int_index)
     @board[int_index] == " " || @board[int_index] == "" ? false : TRUE
   end
+  
+  
   def valid_move?(int_index)
     int_index.between?(0, 8) && !position_taken?(int_index)
   end
@@ -45,12 +57,17 @@ class TicTacToe
     display_board
   end
   
+  
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+  
+  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
+  
+  
   def won?
     WIN_COMBINATIONS.any? do |win_combo|
       if position_taken?(win_combo[0]) && @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]]
@@ -58,22 +75,31 @@ class TicTacToe
       end
     end
   end
+  
+  
   def full? 
     @board.all? {|item| item == "X" || item == "O"}
   end
+  
+  
   def draw?
     full? && !won? ? TRUE : false
   end
+  
+  
   def over?
     won? || full?
   end
+  
+  
   def winner
-   if win_combo = won?
-   @board[win_combo[0]]
- end
+    if win_combo = won?
+      @board[win_combo[0]]
+    end
   end
+  
   def play 
     turn until over?
-  puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
-  end
+      puts winner? "Congratulations #{winner}!" : "Cat's Game!"
+    end
   end
